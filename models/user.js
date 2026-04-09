1bfc6bcca2dd679c388b95f5fa48ff22d47536b3
Jeremy Paule Pereira <86443767+JeremyCodding@users.noreply.github.com>
2026-04-09 14:08:03 +0000

    feat: create `user` model and `/api/v1/users` endpoint

---
+++ models/user.js
@@ -0,0 +1,74 @@
+import database from "infra/database.js";
+import { ValidationError } from "infra/errors";
+
+async function create(userInput) {
+  await validateUniqueEmail(userInput.email);
+  await validateUniqueUsername(userInput.username);
+
+  const newUser = await runInsertQuery(userInput);
+  return newUser;
+
+  async function validateUniqueEmail(email) {
+    const results = await database.query({
+      text: `
+        SELECT
+          email
+        FROM
+          users
+        WHERE
+          LOWER(email) = LOWER($1)
+        ;`,
+      values: [email],
+    });
+
+    if (results.rowCount > 0) {
+      throw new ValidationError({
+        message: "O email informado já está sendo utilizado.",
+        action: "Utilize outro email para realizar o cadastro.",
+      });
+    }
+  }
+
+  async function validateUniqueUsername(username) {
+    const results = await database.query({
+      text: `
+        SELECT
+          username
+        FROM
+          users
+        WHERE
+          LOWER(username) = LOWER($1)
+        ;`,
+      values: [username],
+    });
+
+    if (results.rowCount > 0) {
+      throw new ValidationError({
+        message: "O username informado já está sendo utilizado.",
+        action: "Utilize outro username para realizar o cadastro.",
+      });
+    }
+  }
+
+  async function runInsertQuery(userInput) {
+    const results = await database.query({
+      text: `
+        INSERT INTO
+          users (username, email, password)
+        VALUES
+          ($1, $2, $3)
+        RETURNING
+          *
+        ;`,
+      values: [userInput.username, userInput.email, userInput.password],
+    });
+
+    return results.rows[0];
+  }
+}
+
+const user = {
+  create,
+};
+
+export default user;
